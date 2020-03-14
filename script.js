ddba845f587d40b928dfe1d3c79ce7285a32ffed
patrakovakris <patrakovakris@gmail.com>
2020-03-14 13:49:49 +0400

    feat: add functions for filter images

--- script.js
+++ script.js
@@ -32,5 +32,30 @@ const addTagsClickHandler = () =>{
         tags.querySelectorAll('.btn-nav').forEach( el=> el.classList.remove('active'));
         let clickedTag = event.target;
         activeClikedTag(clickedTag);
+        (event.target.value ==='All') ? showAllImage(): filterImages();
     });
+}
+
+function showAllImage(){
+    let images = document.querySelectorAll('.image');
+    images.forEach((image,i)=>{
+        image.classList.remove('image-hidden');
+    })
+}
+
+function filterImages(){
+    let images = document.querySelectorAll('.image');
+    let random=randomInteger(4,7);
+    images.forEach((image,i)=>{
+        image.classList.remove('image-hidden');
+        if(i%random === 0){
+            console.log(i, random, i%random);
+            image.classList.add('image-hidden');
+        }
+    })
+}
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max - min);
+    return Math.round(rand);
 }
