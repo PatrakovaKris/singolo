04480b08eef1135717af9f95c9444b61c6d5b6a1
patrakovakris <patrakovakris@gmail.com>
2020-03-14 13:32:31 +0400

    feat: add function addNavClickHandler

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+window.onload = function(){
+    console.log('yeeess))')
+    addNavClickHandler();
+}
+
+const addNavClickHandler = () =>{
+    let menu= document.getElementById('header-nav');
+    menu.addEventListener('click', (event)=>{
+        menu.querySelectorAll('a').forEach( el=> el.classList.remove('active'));
+        let clickedTag = event.target;
+        activeClikedTag(clickedTag);
+        // event.preventDefault();
+        addScroll(clickedTag);
+    });
+}
+
+const addScroll = (clickedTag) =>{
+    let link = document.querySelector(clickedTag.getAttribute('href'));
+    link.scrollIntoView( {behavior: "smooth"});
+}
+
+const activeClikedTag = (clickedTag) =>{
+    clickedTag.classList.add('active');
+}
